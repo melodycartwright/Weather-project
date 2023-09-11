7b970b43153ba29662cb265c3a470eeb3bef5380
melodycartwright <melody94c@gmail.com>
2023-09-11 15:10:50 +0200

    Edit Java Script api

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ let days = [
 let day = days[now.getDay()];
 
 h2.innerHTML = `${day}, ${hours}:${minutes}`;
+
 function formatDay(timestamp){
   let date= new Date(timestamp * 1000);
   let day= date.getDay();
@@ -30,23 +31,28 @@ function displayForecast(response){
   let forecastElement= document.querySelector("#forecast");
 
  let forecastHTML= `<div class="row">`;
-  forecast.forEach(function(forecastDay, index) {
+  forecast.forEach(function (forecastDay, index) {
     if(index < 6){
     
-     forecastHTML= forecastHTML + `  <div class="col-2">
-       <div class="weather-forecast-date">${formatDay (forecastDay.dt)}</div>
-       ${index}
-        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>  
-       <div class="weather-forecast-temperatures"> <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+     forecastHTML= 
+     forecastHTML + `  <div class="col-2">
+       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+       
+        <img 
+        src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>  
+
+       <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
          <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
-         </div>  </div>
-               
-          `;}});
+         </div>  
+         </div>
+               `;
+              }
+            });
             forecastHTML= forecastHTML+ `</div>`;
             forecastElement.innerHTML=forecastHTML;
-            console.log(forecastHTML);
+ }
 
-  }
   function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
